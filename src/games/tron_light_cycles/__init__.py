7441bcf155f94224e967aac5f6ef2a6168877126
Sjoegd <sjoerdvdisgames@gmail.com>
2023-09-22 18:55:40 +0200

    switch to godot

--- src/games/tron_light_cycles/__init__.py
+++
@@ -1,8 +0,0 @@
-from gymnasium import register
-from .settings.constants import MAX_TIMESTEPS
-
-register(
-    id='TronLightCycles-v0',
-    entry_point='games.tron_light_cycles.env:TronLightCyclesEnv',
-    max_episode_steps=MAX_TIMESTEPS
-)
