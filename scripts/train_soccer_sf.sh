44fa40848861f82a81f62e2a88c37569d005464a
sjoegd <sjoerdvdisgames@gmail.com>
2023-09-30 13:28:11 +0200

    script + soccer update

---
+++ scripts/train_soccer_sf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gdrl --trainer=sf --env=gdrl --env_path=games/Soccer/build/soccer_train.x86_64 --num_workers=10 --experiment_name=Soccer_0 --speedup=12 --num_policies=1 --env_agents=2 --with_pbt=True --pbt_mix_policies_in_one_env=True --batch_size=2048 --num_batches_per_epoch=2 --num_epochs=10 --learning_rate=0.00003 --exploration_loss_coef=0.001 --lr_schedule=kl_adaptive_epoch --lr_schedule_kl_threshold=0.08 --use_rnn=True --recurrence=32
