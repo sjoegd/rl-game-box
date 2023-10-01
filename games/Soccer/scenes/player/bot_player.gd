c63c59ab4e34defc77fe2b3bea5bfbe7c4e27e8e
Sjoegd <sjoerdvdisgames@gmail.com>
2023-10-01 14:28:04 +0200

    small update

--- games/Soccer/scenes/player/bot_player.gd
+++ games/Soccer/scenes/player/bot_player.gd
@@ -6,6 +6,7 @@ signal touched_ball
 
 @onready var controller = $AIController
 @export var is_left_team: bool = true
+@export var player_overwrite: bool = false
 
 func _process(_delta):
 	if controller.needs_reset:
@@ -15,6 +16,12 @@ func _process(_delta):
 	input_up = controller.go_up
 	input_down = controller.go_down
 	input_kick = controller.go_kick
+	if player_overwrite:
+		input_left = Input.is_action_pressed("left")
+		input_right = Input.is_action_pressed("right")
+		input_up = Input.is_action_pressed("up")
+		input_down = Input.is_action_pressed("down")
+		input_kick = Input.is_action_pressed("kick")
 	super._process(_delta)
 
 func _physics_process(_delta):
