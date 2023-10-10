f7e0dbc617eca1f629c9eb54f1dd85fa53904895
sjoegd <sjoerdvdisgames@gmail.com>
2023-10-10 20:27:28 +0000

    new soccer model

--- scripts/train_soccer_sb3.sh
+++ scripts/train_soccer_sb3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python scripts/sb3_selfplay.py --env_path=games/Soccer/build/soccer_train.x86_64 --save_parent_folder_path=models/Soccer/Soccer_0 \
- --save_model_name=Soccer_0 --speedup=8 --agents_per_env=2 --n_parallel=4 --max_past_agents=25 --total_timesteps=10_000_000\
- --total_iterations=250 --action_repeat=4 
+ --save_model_name=Soccer_0 --speedup=12 --agents_per_env=2 --n_parallel=4 --max_past_agents=25 --total_timesteps=10_000_000\
+ --total_iterations=250 --action_repeat=4 --is_async=True 
