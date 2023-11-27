6627d0c157d214a1092d2828cf8d3dc60ab8af19
sjoegd <sjoerdvdisgames@gmail.com>
2023-11-27 10:40:19 +0000

    added new racing model

--- scripts/train/train_racing.sh
+++ scripts/train/train_racing.sh
@@ -5,5 +5,5 @@ python scripts/sb3_train_single.py \
     --n_parallel=4 \
     --speedup=12 \
     --action_repeat=4 \
-    --timesteps=10_000_000 \
+    --timesteps=2_000_000 \
     --save_model_path="models/Racing/Racing_0"
