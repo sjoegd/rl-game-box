c08786e1273cc128c9b71c2dfe2a1bef5f1c1ed2
Sjoerd van Dis <sjoerdvdisgames@gmail.com>
2023-12-02 14:50:09 +0100

    added training scripts for soccer

---
+++ scripts/test/test_soccer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python scripts/sb3_test.py \
+    --env_path="games/Soccer/builds/soccer_sync.x86_64" \
+    --agents_per_env=2 \
+    --action_repeat=8 \
+    --load_model_paths="models/Soccer/Soccer_0/Soccer_0, models/Soccer/Soccer_0/Soccer_0" \
+    --num_episodes=5 \
