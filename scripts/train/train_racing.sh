a464a57353066cd3aea77b759d6184334aae3514
Sjoerd van Dis <sjoerdvdisgames@gmail.com>
2023-12-14 20:24:49 +0100

    prepared for new racing model (2)

--- scripts/train/train_racing.sh
+++ scripts/train/train_racing.sh
@@ -2,8 +2,8 @@
 
 python scripts/sb3_train.py \
     --env_path="games/Racing/builds/racing_train.x86_64" \
-    --save_parent_folder_path="models/Racing/Racing_2" \
-    --save_model_name="Racing_2" \
+    --save_parent_folder_path="models/Racing/Racing_0" \
+    --save_model_name="Racing_0" \
     --speedup=4 \
     --agents_per_env=4 \
     --games_per_env=2 \
--- scripts/train/train_racing_resume.sh
+++ scripts/train/train_racing_resume.sh
@@ -3,5 +3,5 @@
 python scripts/sb3_train.py \
     --resume_training=True \
     --env_path="games/Racing/builds/racing_train.x86_64" \
-    --save_parent_folder_path="models/Racing/Racing_1" \
-    --save_model_name="Racing_1" \
+    --save_parent_folder_path="models/Racing/Racing_2" \
+    --save_model_name="Racing_2" \
