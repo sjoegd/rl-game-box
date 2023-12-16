c4b56910681692fa1612c6061fb50db9a7cacf5e
Sjoerd van Dis <sjoerdvdisgames@gmail.com>
2023-12-16 09:35:47 +0100

    prepared Racing_4

--- scripts/play/play_racing.sh
+++ scripts/play/play_racing.sh
@@ -2,7 +2,7 @@
 
 python scripts/sb3_play.py \
     --env_path="games/Racing/builds/racing_play.exe" \
-    --model_paths="models/Racing/Racing_3/Racing_3" \
+    --model_paths="models/Racing/Racing_4/Racing_4" \
     --agents_per_env=4 \
     --action_repeat=8 \
     --sessions=5 \
--- scripts/train/train_racing_resume.sh
+++ scripts/train/train_racing_resume.sh
@@ -3,5 +3,5 @@
 python scripts/sb3_train.py \
     --resume_training=True \
     --env_path="games/Racing/builds/racing_train.x86_64" \
-    --save_parent_folder_path="models/Racing/Racing_3" \
-    --save_model_name="Racing_3" \
+    --save_parent_folder_path="models/Racing/Racing_4" \
+    --save_model_name="Racing_4" \
