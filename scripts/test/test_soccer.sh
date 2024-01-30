623f11726f0bbbb4484f4a35ddd30957a94f0bdb
Sjoerd van Dis <sjoerdvdisgames@gmail.com>
2024-01-30 14:14:32 +0100

    Removed shooter

--- scripts/test/test_soccer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python scripts/sb3_test.py \
-    --env_path="games/Soccer/builds/soccer_test.exe" \
-    --model_paths="models/Soccer/Soccer_0/Soccer_0" \
-    --agents_per_env=2 \
-    --action_repeat=8 \
-    --sessions=5 \
