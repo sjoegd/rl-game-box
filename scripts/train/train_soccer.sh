99d3002e6e9993395fd9785e13752db2340249a7
Sjoerd van Dis <sjoerdvdisgames@gmail.com>
2024-02-04 13:54:01 +0100

    small change to soccer train speedup

--- scripts/train/train_soccer.sh
+++ scripts/train/train_soccer.sh
@@ -4,7 +4,7 @@ python scripts/sb3_train.py \
     --env_path="games/Soccer/builds/soccer_train.x86_64" \
     --save_parent_folder_path="models/Soccer/Soccer_0" \
     --save_model_name="Soccer_0" \
-    --speedup=4 \
+    --speedup=5 \
     --agents_per_env=4 \
     --games_per_env=12 \
     --n_parallel=4 \
