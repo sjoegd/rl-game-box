b85e477bd4b3e612ab34192305542857087922d0
Sjoerd van Dis <sjoerdvdisgames@gmail.com>
2024-02-11 11:53:58 +0100

    updated amount of games in training env for soccer

--- scripts/train/train_soccer.sh
+++ scripts/train/train_soccer.sh
@@ -4,11 +4,11 @@ python scripts/sb3_train.py \
     --env_path="games/Soccer/builds/soccer_train.x86_64" \
     --save_parent_folder_path="models/Soccer/Soccer_0" \
     --save_model_name="Soccer_0" \
-    --speedup=12 \
+    --speedup=8 \
     --agents_per_env=4 \
-    --games_per_env=1 \
+    --games_per_env=4 \
     --n_parallel=4 \
     --action_repeat=8 \
     --max_past_agents=25 \
-    --total_timesteps=10_000_000 \
-    --total_iterations=100 \
+    --total_timesteps=25_000_000 \
+    --total_iterations=250 \
