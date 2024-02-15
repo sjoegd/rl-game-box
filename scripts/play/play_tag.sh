bb7104f61beafee6ba0f00e69c2ef6abb4e78ac4
Sjoerd van Dis <sjoerdvdisgames@gmail.com>
2024-02-15 19:31:53 +0100

    removed tag

--- scripts/play/play_tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python scripts/sb3_play.py \
-    --env_path="games/Tag/builds/tag_play.exe" \
-    --model_paths="models/Tag/Tag_0/Tag_0" \
-    --agents_per_env=4 \
-    --action_repeat=8 \
-    --sessions=5 \
